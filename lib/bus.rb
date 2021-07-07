9c3916182cf3393603d4452456cd4193722b138e
Tanner Dale <tanner@tannerdale.com>
2021-07-07 15:34:38 -0500

    Add iteration 1 bus class

---
+++ lib/bus.rb
@@ -0,0 +1,17 @@
+class Bus
+  attr_reader :name, :capacity, :passengers
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @passengers = []
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def yell_at_passengers
+    @passengers.map(&:upcase)
+  end
+end
